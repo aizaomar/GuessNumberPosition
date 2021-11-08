b6ed41996f1c5eaf8b99b029aa0b0cc6e47f419d
aizaomar <w.omaraiza@gmail.com>
2021-11-08 18:36:03 +0600

    Initial Commit

---
+++ GuessNumberPosition/GuessNumberPositionApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessNumberPositionApp.swift
+//  GuessNumberPosition
+//
+//  Created by Aiza on 08.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessNumberPositionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
