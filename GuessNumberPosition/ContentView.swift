5ea4ded529a2a0d9fe2aa85edf217e7009f17f85
aizaomar <w.omaraiza@gmail.com>
2021-11-09 21:13:21 +0600

    Sample UI

--- GuessNumberPosition/ContentView.swift
+++ GuessNumberPosition/ContentView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var currentValue: Double = 50
+    @State private var targetValue: Int = 35
+    
+    @State private var showAlert =  false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("Подвинь слайдер, как можно ближе к: \(targetValue)")
+            
+//             UIKit Slider .........$currentValue, $target bla bla...
+            
+          // вынести кнопки
+            VStack(spacing: 30) {
+                Button(action: checkValue) {
+                    Text("Проверь меня")
+                }
+                .alert(Text("Ваш счет \(computeScore()) из 100"), isPresented: $showAlert) {
+                }
+                Button(action: restartGame) {
+                    Text("Начать заново")
+                }
+            }
+            
+        }.padding()
+    }
+    
+    private func checkValue() {
+        showAlert.toggle()
+    }
+    
+    private func restartGame() {
+        targetValue = Int.random(in: 0...100)
+    }
+    
+    private func computeScore() -> Int {
+        let difference  = abs(targetValue - lround(currentValue))
+        return 100 - difference
     }
 }
 
@@ -19,3 +54,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
