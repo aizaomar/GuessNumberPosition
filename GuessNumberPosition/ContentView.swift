cf4c4d8efc9c3452eb8b87892e5cbbacac276ae6
aizaomar <w.omaraiza@gmail.com>
2021-11-09 23:13:43 +0600

    Fix content view

--- GuessNumberPosition/ContentView.swift
+++ GuessNumberPosition/ContentView.swift
@@ -8,51 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var currentValue: Double = 50
-    @State private var targetValue: Int = 35
+    @State private var currentValue: Double = 0.5
+    @State private var targetValue: Int = Int.random(in: 0...100)
+    @State private var opacity: Double = 1.0
     
     @State private var showAlert =  false
     
     var body: some View {
-        VStack{
+        VStack {
             Text("Подвинь слайдер, как можно ближе к: \(targetValue)")
-            
-//             UIKit Slider .........$currentValue, $target bla bla...
-            
-          // вынести кнопки
-            VStack(spacing: 30) {
-                Button(action: checkValue) {
-                    Text("Проверь меня")
-                }
-                .alert(Text("Ваш счет \(computeScore()) из 100"), isPresented: $showAlert) {
-                }
-                Button(action: restartGame) {
-                    Text("Начать заново")
-                }
-            }
-            
+            SliderView(
+                opacity: $opacity,
+                currentValue: $currentValue,
+                targetValue: $targetValue
+            )
+            ButtonView(
+                opacity: opacity,
+                showAlert: $showAlert,
+                targetValue: $targetValue
+            )
         }.padding()
     }
-    
-    private func checkValue() {
-        showAlert.toggle()
-    }
-    
-    private func restartGame() {
-        targetValue = Int.random(in: 0...100)
-    }
-    
-    private func computeScore() -> Int {
-        let difference  = abs(targetValue - lround(currentValue))
-        return 100 - difference
-    }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
-
-
